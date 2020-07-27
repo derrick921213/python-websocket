f6a96b1b26efea5babd275f9c072e1952de02ecf
derrick921213 <dlin12457@gmail.com>
2020-07-27 12:43:48 +0800

    ch

--- test/server.py
+++ test/server.py
@@ -1,6 +1,5 @@
-from websocket_server import WebsocketServer
+'''from websocket_server import WebsocketServer
 import sys
-#sys.setdefaultencoding('utf-8')
 def new_client(client,server):
     print("Client(%d) has joined."% client['id'])
 def client_left(client,server):
@@ -13,4 +12,34 @@ server = WebsocketServer(4200,host='')
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_back)
+server.run_forever()'''
+from websocket_server import WebsocketServer
+import sys
+print(sys.getdefaultencoding())
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+    print("New client connected and was given id %d" % client['id'])
+    server.send_message_to_all("Hey all, a new client has joined us")
+
+
+# Called for every client disconnecting
+def client_left(client, server):
+    print("Client(%d) disconnected" % client['id'])
+
+
+# Called when a client sends a message
+def message_received(client, server, message):
+    if len(message) > 200:
+        message = message[:200]+'..'
+    type = sys.getfilesystemencoding()    
+    print("Client(%d) said: %s" % (client['id'], message))
+    server.send_message_to_all("Client(%d) said: %s" % (client['id'], message))
+    
+
+
+PORT=4200
+server = WebsocketServer(PORT)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
 server.run_forever()
